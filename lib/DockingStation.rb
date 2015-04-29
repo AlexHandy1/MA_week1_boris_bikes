f489003b89cd4286484d5d6b8b591c646b776276
Alex Handy <handy437@gmail.com>
2015-04-29 19:40:04 +0100

    First effort Stage 5

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,13 +2,33 @@ require_relative 'Bike'
 
 class DockingStation
 
-	def release_bike
-		raise "No bikes available" unless @bike
-		@bike
+	DEFAULT_CAPACITY = 20
+
+	def initialize
+		@bikes = []
 	end
 
 	def dock bike
-		@bike = bike
+		fail "Station Full" if full?
+		bikes << bike
+		nil
+	end
+
+	def release_bike
+		fail "No bikes available" if empty?
+		bikes.pop
+	end
+
+	private 
+
+	attr_accessor :bikes
+
+	def full?
+		bikes.length >= 20
+	end
+
+	def empty?
+		bikes.empty?
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,17 @@ describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
 	it { is_expected.to respond_to(:dock).with(1).argument}
 
+	it 'can dock a bike' do
+		expect(subject.dock double :bike).to be nil
+	end
+
 	describe "release_bike" do
 		it 'raises an error when there is no bike available' do
 		expect {subject.release_bike}.to raise_error "No bikes available"
+	end
+	it 'raises an error when full' do 
+		20.times { subject.dock double :bike }
+		expect {subject.dock double :bike}.to raise_error "Station Full"
 	end
   end 
 end
