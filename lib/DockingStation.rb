a9511376479173e7f2adb37641a9950145452ab1
Alex Handy <handy437@gmail.com>
2015-04-30 11:14:08 +0100

    Made bike array pass

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,13 +1,18 @@
 require_relative 'Bike'
 
 class DockingStation
+
+	def initialize
+		@bikes = []
+	end
+
 	def dock bike
-		fail "Docking Station Full" if @bike
-		@bike = bike
+		fail "Docking Station Full" if @bikes.count >= 20
+		@bikes << bike
 	end
 
 	def release_bike
-		fail "No Bikes Available" unless @bike
-		@bike
+		fail "No Bikes Available" if @bikes.empty?
+		@bikes.pop
 	end
 end
